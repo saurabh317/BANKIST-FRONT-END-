4d6f2191648db0d4660ceae89ac5f1e76be5c050
saurabh317 <saurabhsp317@gmail.com>
2022-12-20 20:04:45 +0530

    smooth scroll

--- JS/script1.js
+++ JS/script1.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 ///////////////////////////////////////
 // Modal window
 
@@ -31,3 +33,46 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//smooth scroll----
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+btnScrollTo.addEventListener('click', function(e){
+const sectionScroll = document.querySelector("#section--1");
+sectionScroll.scrollIntoView({
+  behavior:'smooth'
+});
+})
+
+
+//smooth scroll to the nav bar
+
+// const navLinks = document.querySelectorAll('.nav__link');
+// navLinks.forEach(function(ele){
+//   ele.addEventListener("click",function(e){
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior:"smooth"
+//     }) 
+//   })
+
+// })
+// if we do by the above code here problem is that if we will be having 1000 elements then simply 1000 copies of that function will get created so here comes the EVENT DELIGATION
+
+//EVENT DELIGATION---
+
+const navLink = document.querySelector('.nav__links');
+navLink.addEventListener('click',function(e){
+  e.preventDefault();
+  //matching the links
+  if(e.target.classList.contains("nav__link")){
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior:"smooth"
+    })
+
+  }
+ 
+})
